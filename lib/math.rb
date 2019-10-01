24f8d97ff73ac93c6d53dcf1884f5f82566cc8ee
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-01 05:13:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,15 +2,15 @@ def addition(5, 4).to eq(9)
 
 end
 
-def subtraction(num1, num2)
+def subtraction(10, 2).to eq(20)
 
 end
 
-def division(num1, num2)
+def division(50, 5).to eq(10)
 
 end
 
-def multiplication(num1, num2)
+def multiplication(5, 5).to eq(25)
 
 end
 
